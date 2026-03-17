433882ff44d9829e3efc4a0374bb68ec884a6238
johannes-christian.schuster <johannes-christian.schuster@stud.uni-bamberg.de>
2026-03-17 14:02:33 +0100

    first commit

---
+++ lib/shot_ds.ex
@@ -0,0 +1,18 @@
+defmodule ShotDs do
+  @moduledoc """
+  Documentation for `ShotDs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShotDs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shot_ds_test.exs
@@ -0,0 +1,8 @@
+defmodule ShotDsTest do
+  use ExUnit.Case
+  doctest ShotDs
+
+  test "greets the world" do
+    assert ShotDs.hello() == :world
+  end
+end
